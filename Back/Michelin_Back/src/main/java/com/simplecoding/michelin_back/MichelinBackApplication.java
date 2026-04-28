c5a797c1efa264739e645c1b90b6a79de7b7ae56
Woogi7859 <zxcvbnmk8867@gmail.com>
2026-04-28 12:45:53 +0900

    [템플릿]기초 템플릿 제작
    [템플릿]기초 템플릿 제작

---
+++ Back/Michelin_Back/src/main/java/com/simplecoding/michelin_back/MichelinBackApplication.java
@@ -0,0 +1,13 @@
+package com.simplecoding.michelin_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MichelinBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MichelinBackApplication.class, args);
+    }
+
+}
---
+++ Back/Michelin_Back/src/test/java/com/simplecoding/michelin_back/MichelinBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplecoding.michelin_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MichelinBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
